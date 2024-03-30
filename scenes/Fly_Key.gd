3d7a7ee5eab40cf742f79be8eb37fbbd946ca015
Shorif <shorif@caffebd.org>
2024-03-30 15:33:47 +0600

    flycode

--- scenes/Fly_Key.gd
+++ scenes/Fly_Key.gd
@@ -2,8 +2,8 @@ extends Area2D
 
 
 
-func _on_Player_body_entered(body):
-	if body.has_method("eat"):
-		body.eat()
-	queue_free() 
 
+
+func _on_Fly_Key_body_entered(body):
+	if body.is_in_group("player"):
+		queue_free()
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,16 +4,25 @@ var FLY_SPEED = 200
 var GRAVITY = 500
 var FLY_ACCELERATION = 1000
 export var speed = 250
+var canFly = false
+
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+
+
+	move_and_slide(velocity)
+
+
 
 export var jump_speed = -450
 var gravity = 900
 
 var direction := Vector2.ZERO
 
-var velocity = Vector2()
 
-
-# Called when the node enters the scene tree for the first time
 
 func _input(event):
 	
@@ -28,7 +37,7 @@ func _input(event):
 		direction.x -= speed
 		$PlayerAnim.flip_h = true
 
-#	NEED MOVE LEFT CODE
+
 
 func _process(delta):
 	
@@ -41,7 +50,7 @@ func _process(delta):
 
 func _physics_process(delta):
 	if not Input.is_action_pressed("fly"):
-		velocity.y += GRAVITY * delta
+		velocity.y += gravity * delta
 
 
 
